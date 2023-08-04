477d8653077d478ec3bf6d97b1ff12f4b1121b6a
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-08-04 15:17:40 +0200

    some light adjustments

--- Eventuality/src/main/java/com/eventuality/pages/Home.java
+++ Eventuality/src/main/java/com/eventuality/pages/Home.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.eventuality.pages;
 
-/**
- *
- * @author logan
- */
 public class Home extends javax.swing.JFrame {
 
     /**
--- Eventuality/src/main/java/com/eventuality/pages/Login.java
+++ Eventuality/src/main/java/com/eventuality/pages/Login.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.eventuality.pages;
 
-/**
- *
- * @author logan
- */
 public class Login extends javax.swing.JFrame {
 
     /**
