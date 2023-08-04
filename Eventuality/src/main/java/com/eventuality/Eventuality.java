159a2a5bc9729fa8a52722d524af471c4e1c7508
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-08-04 12:31:09 +0200

    test

--- Eventuality/src/main/java/com/eventuality/Eventuality.java
+++ Eventuality/src/main/java/com/eventuality/Eventuality.java
@@ -10,6 +10,7 @@ import com.eventuality.pages.Home;
 public class Eventuality {
 
     public static void main(String[] args) {
+        //display homepage
         Home firstPage = new Home();
         firstPage.setVisible(true);
     }
