b837756afa55078ccaf3fc812de1dcc39a915d3c
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-08-04 16:44:12 +0200

    DBConnetion setup
    set up the file to handle connecting to the database

--- Eventuality/src/main/java/com/eventuality/controls/DbConnect.java
+++ Eventuality/src/main/java/com/eventuality/controls/DbConnect.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.eventuality.controls;
 
-/**
- *
- * @author logan
- */
+import java.sql.*;
+
 public class DbConnect {
     
+    //please note the URL will change on windows so we need to remember
+    private String dbURL = "/home/logan/Documents/GitHub/EVENTuality/Database/Eventuality";
+    
+    private Connection ConnectToDB() throws SQLException{
+        return DriverManager.getConnection(dbURL);
+    }
+    
+    public Statement StatementCreate() throws SQLException{
+        return ConnectToDB().createStatement();
+    }
+    
+    public void CloseAll(Statement s) throws SQLException{
+        s.close();
+        ConnectToDB().close();
+    }
 }
