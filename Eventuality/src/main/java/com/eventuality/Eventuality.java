9825293d4e0afccc516d3e0c58074d663e7463ba
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-08-04 18:43:06 +0200

    drivers not found
    cant seem to get the drivers to pick up for embedded drivers

--- Eventuality/src/main/java/com/eventuality/Eventuality.java
+++ Eventuality/src/main/java/com/eventuality/Eventuality.java
@@ -1,15 +1,27 @@
-
 package com.eventuality;
 
 
+import com.eventuality.controls.DbConnect;
 import com.eventuality.pages.Home;
+import java.sql.*;
 
 public class Eventuality {
 
     public static void main(String[] args) {
         //display homepage
-       Home firstPage = new Home();
-       firstPage.setVisible(true);
+       //Home firstPage = new Home();
+       //firstPage.setVisible(true);
      
+       try{
+           DbConnect db = new DbConnect();
+           System.out.println("making Connection");
+           Statement s = db.StatementCreate();
+           System.out.println("Connection made.. Statement made...");
+           db.CloseAll(s);
+           System.out.println("terminated...");
+           
+       }catch(Exception e){
+           System.out.println("Err: " + e.getMessage());
+       }
     }
 }
