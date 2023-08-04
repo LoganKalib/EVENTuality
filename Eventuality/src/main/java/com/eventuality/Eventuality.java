18f6b2a1f36eba2df89a4e35f907521f393b6fdc
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-08-04 10:59:18 +0200

    base project, and database
    set up basic package structure, and started the database. made the project homepage load on start

---
+++ Eventuality/src/main/java/com/eventuality/Eventuality.java
@@ -0,0 +1,16 @@
+
+package com.eventuality;
+
+import com.eventuality.pages.Home;
+
+/**
+ *
+ * @author logan
+ */
+public class Eventuality {
+
+    public static void main(String[] args) {
+        Home firstPage = new Home();
+        firstPage.setVisible(true);
+    }
+}
