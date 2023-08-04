f6ecc2e82c27b9115275e1f68a0b864c906d708d
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-08-04 19:16:21 +0200

    Update Eventuality.java

--- Eventuality/src/main/java/com/eventuality/Eventuality.java
+++ Eventuality/src/main/java/com/eventuality/Eventuality.java
@@ -1,7 +1,5 @@
 package com.eventuality;
 
-
-import com.eventuality.controls.DbConnect;
 import com.eventuality.pages.Home;
 import java.sql.*;
 
@@ -9,19 +7,7 @@ public class Eventuality {
 
     public static void main(String[] args) {
         //display homepage
-       //Home firstPage = new Home();
-       //firstPage.setVisible(true);
-     
-       try{
-           DbConnect db = new DbConnect();
-           System.out.println("making Connection");
-           Statement s = db.StatementCreate();
-           System.out.println("Connection made.. Statement made...");
-           db.CloseAll(s);
-           System.out.println("terminated...");
-           
-       }catch(Exception e){
-           System.out.println("Err: " + e.getMessage());
-       }
+       Home firstPage = new Home();
+       firstPage.setVisible(true);
     }
 }
