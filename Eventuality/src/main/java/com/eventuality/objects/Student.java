c639aa4c666beaeb29d9a7e891d9df8dee9956d0
SheldonArendse <126950809+SheldonArendse@users.noreply.github.com>
2023-08-06 18:39:24 +0200

    Adding student object
    adding missing student object from initial push of objects

--- Eventuality/src/main/java/com/eventuality/objects/Student.java
+++ Eventuality/src/main/java/com/eventuality/objects/Student.java
@@ -1,5 +1,57 @@
 package com.eventuality.objects;
 
 public class Student {
-    
+    private String studName, studSurname, studPassword, studEmail;
+
+    public Student(String studName, String studSurname, String studPassword, String studEmail) {
+        this.studName = studName;
+        this.studSurname = studSurname;
+        this.studPassword = studPassword;
+        this.studEmail = studEmail;
+    }
+
+    public Student() {
+        this.studName = "";
+        this.studSurname = "";
+        this.studPassword = "";
+        this.studEmail = "";
+    }
+
+    public String getStudName() {
+        return studName;
+    }
+
+    public String getStudSurname() {
+        return studSurname;
+    }
+
+    public String getStudPassword() {
+        return studPassword;
+    }
+
+    public String getStudEmail() {
+        return studEmail;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public void setStudSurname(String studSurname) {
+        this.studSurname = studSurname;
+    }
+
+    public void setStudPassword(String studPassword) {
+        this.studPassword = studPassword;
+    }
+
+    public void setStudEmail(String studEmail) {
+        this.studEmail = studEmail;
+    }
+
+    @Override
+    public String toString() {
+        return this.studName + "\n" + this.studSurname + "\n" + 
+                this.studPassword + "\n" + this.studEmail;
+    }
 }
