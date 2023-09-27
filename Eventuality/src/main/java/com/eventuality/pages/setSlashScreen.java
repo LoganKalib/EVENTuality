d9783a28c0de8e181b5a9fba0996544e1a1bd3e1
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-09-27 12:13:33 +0200

    Update setSlashScreen.java

--- Eventuality/src/main/java/com/eventuality/pages/setSlashScreen.java
+++ Eventuality/src/main/java/com/eventuality/pages/setSlashScreen.java
@@ -9,14 +9,14 @@ public class setSlashScreen extends JWindow {
     public setSlashScreen(){
         
         content = new JPanel(new BorderLayout());
-        content.setBackground(Color.cyan);
+        content.setBackground(Color.CYAN);
         ImageIcon logo = new ImageIcon("/home/logan/Documents/GitHub/EVENTuality/Images/logo (1).png"); // Replace with your image file
         content.add(new JLabel(logo), BorderLayout.CENTER);
         JLabel loadingLabel = new JLabel("Loading...");
         loadingLabel.setHorizontalAlignment(SwingConstants.CENTER);
         content.add(loadingLabel, BorderLayout.SOUTH);
         setContentPane(content);
-        pack();
+        setSize(600, 400);
 
         // Center the splash screen on the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
